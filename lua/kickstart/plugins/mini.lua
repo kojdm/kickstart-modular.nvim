9fcff720799df794967e0d2b4b004330d81ab94d
kojidelmundo <kojidelmundo@gmail.com>
2025-07-31 21:29:39 +0800

    Customize mini statusline

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -26,9 +26,11 @@ return {
         use_icons = vim.g.have_nerd_font,
         content = {
           active = function()
-            local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
-            local filename = MiniStatusline.section_filename { trunc_width = 140 }
+            local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 99999 } -- force short output
+            local filename = MiniStatusline.section_filename { trunc_width = 99999 } -- force short output
             local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
+            local location = MiniStatusline.section_location { trunc_width = 75 }
+            local search = MiniStatusline.section_searchcount { trunc_width = 75 }
 
             return MiniStatusline.combine_groups {
               { hl = mode_hl, strings = { mode } },
@@ -36,6 +38,14 @@ return {
               { hl = 'MiniStatuslineFilename', strings = { filename } },
               '%=', -- End left alignment
               { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+              { hl = mode_hl, strings = { search, location } },
+            }
+          end,
+          inactive = function()
+            local filename = MiniStatusline.section_filename { trunc_width = 99999 } -- force short output
+
+            return MiniStatusline.combine_groups {
+              { hl = 'MiniStatuslineFilename', strings = { filename } },
             }
           end,
         },
